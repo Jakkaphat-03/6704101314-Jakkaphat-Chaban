963512943993e982a0c85d3e3dfb8090b42aca08
Jakkaphat-03 <audijakkapat03@gmail.com>
2025-10-08 11:08:07 +0700

    Form testing

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,51 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="name"
+        label="Your name"
+        :rules="[val => !!val || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        v-model="age"
+        label="Your age"
+        type="number"
+        :rules="[
+          val => !!val || 'Please type your age',
+          val => val > 0 || 'Please type a real age'
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="secondary" flat />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const name = ref('')
+const age = ref(null)
+const accept = ref(false)
+
+function onSubmit () {
+  if (accept.value) {
+    alert('Submitted!')
+  }
+}
+
+function onReset () {
+  name.value = ''
+  age.value = null
+  accept.value = false
+}
 </script>
+
